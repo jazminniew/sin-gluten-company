b65cda1e7b58164593e3dfa57de4263b92f95bab
jazminniew <jazminniew@gmail.com>
2025-04-04 18:15:19 -0300

    logica del excel funcionando para nuestros locales

--- src/components/OurLocals.js
+++ src/components/OurLocals.js
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as XLSX from "xlsx";
 import LocalCard from "./LocalCard";
 import "./OurLocals.css";
 
-const localsData = [
-  { name: "Local 1", image: "ruta-del-logo-amaranto.png", link: "https://instagram.com" },
-  { name: "Local 2", image: "ruta-del-logo-local2.png", link: "https://instagram.com" },
-  { name: "Local 3", image: "ruta-del-logo-local3.png", link: "https://instagram.com" },
-  { name: "Local 4", image: "ruta-del-logo-local4.png", link: "https://instagram.com" },
-  { name: "Local 5", image: "ruta-del-logo-amaranto.png", link: "https://instagram.com" },
-  { name: "Local 2", image: "ruta-del-logo-local2.png", link: "https://instagram.com" },
-  { name: "Local 3", image: "ruta-del-logo-local3.png", link: "https://instagram.com" },
-  { name: "Local 4", image: "ruta-del-logo-local4.png", link: "https://instagram.com" },
-  { name: "Local 5", image: "ruta-del-logo-amaranto.png", link: "https://instagram.com" },
-  { name: "Local 2", image: "ruta-del-logo-local2.png", link: "https://instagram.com" },
-  { name: "Local 3", image: "ruta-del-logo-local3.png", link: "https://instagram.com" },
-  { name: "Local 4", image: "ruta-del-logo-local4.png", link: "https://instagram.com" },
-  // Agregar más datos de locales aca...
-];
-
 const OurLocals = () => {
+  const [localsData, setLocalsData] = useState([]);
+
+  useEffect(() => {
+    fetch("/SGCDB.xlsx")
+      .then((res) => res.arrayBuffer())
+      .then((buffer) => {
+        const workbook = XLSX.read(buffer, { type: "array" });
+        const sheetName = workbook.SheetNames[0];
+        const rawData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+        const mappedData = rawData.filter(item => item.nombreLogo).map(item => ({
+          name: item.nombreLogo,
+          image: item.logoCircular,
+          link: item.linkLogo,
+        }));
+        
+        setLocalsData(mappedData);
+      });
+  }, []);
+
   return (
     <div className="component">
       <div className="our-locals">
         <div className="locals-grid">
           {localsData.map((local, index) => (
-            <LocalCard key={index} name={local.name} image={local.image} link={local.link} />
+            <LocalCard
+              key={index}
+              name={local.name}
+              image={local.image}
+              link={local.link}
+            />
           ))}
         </div>
       </div>
